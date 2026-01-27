55c391b8f0652825741c290de3fe7dc0426e4575
Renan Duarte <38355185+r3na@users.noreply.github.com>
2026-01-27 09:26:24 +0100

    Update lambda_function.py

--- aws-lambda/lambda_function.py
+++ aws-lambda/lambda_function.py
@@ -8,9 +8,10 @@ import base64
 # variables
 C_SEC_CODE = '***'
 C_UUID_BELL = '***'
-C_SENDER_EMAIL = 'info@list.com'
+C_SENDER_EMAIL = 'info@***'
 C_AWS_REGION = "eu-central-1"
 C_WHATSAPP_SENDER_ID = '***'
+C_WHATSAPP_TEMPLATE_NAME = '***'
 C_SMS_SENDER_ID = '***'
 
 # Initialize SNS client
@@ -63,15 +64,16 @@ def send_phone_sms_message(sender_id, phone_number, hourminute):
                 }
                 })
 
-def send_phone_whatsapp_message(sender_id, phone_number, hourminute):
+def send_phone_whatsapp_message(destination_phone_number, msg):
     body = {
         "messaging_product": "whatsapp",
-        "to": phone_number,
+        "recipient_type": "individual",
+        "to": destination_phone_number,
         "type": "template",
         "template": {
-            "name": "bell_notification",
+            "name": C_WHATSAPP_TEMPLATE_NAME,
             "language": {
-                "code": "en_US"
+                "code": "en"
             },
             "components": [
                 {
@@ -79,7 +81,7 @@ def send_phone_whatsapp_message(sender_id, phone_number, hourminute):
                     "parameters": [
                         {
                             "type": "text",
-                            "text": hourminute
+                            "text": str(msg)
                         }
                     ]
                 }
@@ -87,12 +89,12 @@ def send_phone_whatsapp_message(sender_id, phone_number, hourminute):
         }
     }
     json_bytes = json.dumps(body).encode('utf-8')
-    base64_bytes = base64.b64encode(json_bytes)    
+    
     return socmes_client.send_whatsapp_message(
-        originationPhoneNumberId= C_WHATSAPP_SENDER_ID, 
-        message= json_bytes,
-        metaApiVersion="v20.0"
-        )
+        originationPhoneNumberId=C_WHATSAPP_SENDER_ID,
+        message=json_bytes,
+        metaApiVersion="v21.0"
+    )
 
 def get_subject(action, hourminute):
     if action.upper().strip() == 'BELL':
@@ -148,8 +150,10 @@ def lambda_handler(event, context):
         if uuid == C_UUID_BELL:
             # sms
             for phone_item in phone_list:
-                # phone (e.g. sms)
+                # phone (sms)
                 response.append(send_phonemessage(sender_id, phone_item, hourminute))
+                # phone (whatsapp)
+                response.append(send_phone_whatsapp_message(phone_item, dateandhour))
             if only_phone.lower().strip() == 'no':
                 # email
                 for email_item in email_list:
